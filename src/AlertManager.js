ac29d4da882320f97a09655bdd749a9d6ecdfebe
Doug Perez <perezcpt@gmail.com>
2022-07-28 17:07:53 -0700

    react hook form added

---
+++ src/AlertManager.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+
+export default function Alerts() {
+  return (
+    <Stack
+      sx={{ width: "25%", position: "absolute", top: 15, right: 15 }}
+      spacing={2}
+    >
+      <Alert severity="error">This is an error alert — check it out!</Alert>
+      <Alert severity="warning">This is a warning alert — check it out!</Alert>
+      <Alert severity="info">This is an info alert — check it out!</Alert>
+      <Alert severity="success">This is a success alert — check it out!</Alert>
+    </Stack>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Alerts from "./AlertManager.js";
+import AlertExample from "./AlertExample.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Alerts />
+        <br />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <AlertExample />
       </header>
     </div>
   );
